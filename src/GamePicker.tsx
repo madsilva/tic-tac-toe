886b6ad66b56f96c7427b35839efc71014737936
Madeline Silva <madsilva@Madelines-MacBook-Air.local>
2025-09-25 17:53:57 -0400

    add links to games on lobby page

--- src/GamePicker.tsx
+++ src/GamePicker.tsx
@@ -1,24 +1,35 @@
-import { useState, useEffect } from 'react'
+import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query";
+import { type GameState } from './gamelogic'
+import { Link } from 'react-router';
 
 export const GamePicker = () => {
-  const [games, setGames] = useState({})
+  const queryClient = useQueryClient()
 
-  useEffect( () => {
-    const fetchGames = async () => {
-      const res  = await fetch('/list', {method: 'GET'})
-      const data = await res.json()
-      console.log(data)
-    }
-    fetchGames()
-  }, [])
+  const getGames = async () => {
+    const result = await fetch('/list', { method: 'GET' })
+    const json_result = await result.json()
+    console.log("getGames result", json_result)
+    return json_result
+  }
+
+  const { isPending, error, data } = useQuery({queryKey: ['games'], queryFn: getGames})
+  console.log("this is the data", data)
   
   const handleNewGame = async () => {
     const res = await fetch('/create', {method: 'POST', headers: { 'Content-Type': 'application/json' } })
   }
   
-  return (
-    <>
-      <button onClick={handleNewGame}>new game!!!!</button>
-    </>
-  )
+  console.log('data as list?', Object.entries(data))
+  if (isPending) {
+    return <p>loading....</p>
+  } else {
+      return (
+          
+          <>
+            {Object.entries(data).map(([id, game]) => <div><Link to={`/game/${game.id}`}>{game.id}</Link></div>)}
+            <button onClick={handleNewGame}>new game!!!!</button>
+          </>
+        )
+  }
+  
 }
